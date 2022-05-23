ecb562650b2bfc885424422db8fed385160e2994
Asger Asgerov <asgaraa@code.edu.az>
2022-05-23 18:18:35 +0400

    CreateLoginREgisterService

--- FinalApi/Api/Controllers/AccountController.cs
+++ FinalApi/Api/Controllers/AccountController.cs
@@ -1,70 +1,36 @@
-﻿using DomainLayer.Entities;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using ServiceLayer.DTOs.AppUser;
 using ServiceLayer.Services.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+
 
 namespace Api.Controllers
 {
     public class AccountController : BaseController
     {
-
-        private readonly UserManager<AppUser> _userManager;
-        private readonly RoleManager<IdentityRole> _roleManager;
-        private readonly ITokenService _tokenService;
-        public AccountController(UserManager<AppUser> userManager,
-                                 RoleManager<IdentityRole> roleManager,
-                                 ITokenService tokenService)
+        private readonly IAccountService _service;
+        public AccountController(IAccountService service)
         {
-            _userManager = userManager;
-            _roleManager = roleManager;
-            _tokenService = tokenService;
+            _service = service;
         }
+
 
         [HttpPost]
         [Route("Register")]
         public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
         {
-            AppUser user = new()
-            {
-                Name = registerDto.Name,
-                Email = registerDto.Email,
-                Surname = registerDto.Surname
-            };
-
-            await _userManager.CreateAsync(user, registerDto.Password);
-            // await _userManager.AddToRoleAsync(user, "admin");
+            await _service.Regsiter(registerDto);
             return Ok();
         }
 
-        [HttpPost]
-        [Route("CreateRole")]
-        public async Task<IActionResult> CreateRole([FromBody] RoleDto roleDto)
-        {
-            await _roleManager.CreateAsync(new IdentityRole { Name = roleDto.Role });
-            return Ok();
-        }
+
 
 
         [HttpPost]
         [Route("Login")]
-        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        public async Task<string> Login([FromBody] LoginDto loginDto)
         {
-            var user = await _userManager.FindByEmailAsync(loginDto.Email);
-
-            if (user is null) return NotFound();
-
-            if (!await _userManager.CheckPasswordAsync(user, loginDto.Password)) return Unauthorized();
-
-            var roles = await _userManager.GetRolesAsync(user);
-
-            string token = _tokenService.GenerateJwtToken(user.UserName, (List<string>)roles);
-
-            return Ok(token);
+            return await _service.Login(loginDto);
         }
     }
 }
